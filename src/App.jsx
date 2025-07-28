e99f1718749a2f0f257a848cbd5942ed1f850602
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-28 19:43:56 -0300

    configurações tailwind e inicio projeto to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from "react"
+import { useState } from "react";
+import AddTask from "./components/AddTask"
+import Tasks from "./components/Tasks"
 
 function App(){
-  const [message, setMessage] = useState("Olá, mundo!")
+  const [tasks, setTasks] = useState([{
+    id:1,
+    title: "Estudar Programação",
+    description: "Estudar Programação para obter conhecimento em tecnologia",
+    isCompleted: false
+  },
+  {
+    id:2,
+    title: "Estudar Ingles",
+    description: "Estudar ingles para obter conhecimento em línguas",
+    isCompleted: false
+  },
+  {
+    id:3,
+    title: "Estudar matemática",
+    description: "Estudar matemática para obter conhecimento em cálculo",
+    isCompleted: false
+  }
+
+])
   return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={()=>{
-        setMessage("Olá, fui clicado!")
-      }}>Mudar mensagem</button>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <AddTask />
+        <Tasks tasks={tasks}/>
+      </div>
     </div>
-  )
-  
+  );
 }
 
 export default App
