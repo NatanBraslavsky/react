de0a1456499a289c11f71c675414adc403a31413
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-09-03 17:51:47 -0300

    renderização condicional react

--- cursos/yt-50-curso/src/App.jsx
+++ cursos/yt-50-curso/src/App.jsx
@@ -2,11 +2,19 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Products";
 
+const ValidPassword = () => <h1>Valid Password</h1>;
+const InvalidPassword = () => <h1>Invalid Password</h1>
+
+const Password = ({isValid}) => {
+  return isValid ? <ValidPassword/> : <InvalidPassword/>
+}
+
 export default function App() {
     return (
         <div>
             <Header />
             <Main />
+            <Password isValid={true}/>
             <Footer />
         </div>
     );
--- cursos/yt-50-curso/src/components/Products.jsx
+++ cursos/yt-50-curso/src/components/Products.jsx
@@ -4,10 +4,10 @@ const Products = () => {
         <div className="flex gap-2 m-3">
             {product.map((prod)=>{
                 return(
-                    <div className="text-white bg-green-400 mb-4 rounded w-96 text-center p-3">
+                    <div className=" bg-green-400 mb-4 rounded w-96 text-center p-3">
                         <h1>Nome: {prod.nome}</h1>
                         <h1>Id: {prod.id}</h1>
-                        <h1>Preco: {prod.preco}</h1>
+                        <h1>Preço: {prod.preco}</h1>
                     </div>
                 )
             })}
