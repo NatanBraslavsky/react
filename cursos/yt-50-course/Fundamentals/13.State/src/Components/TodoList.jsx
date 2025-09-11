5e5a349d0b03244e238cedf86d12fd774c2aa054
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-09-11 09:36:10 -0300

    refiz para revisar

--- cursos/yt-50-course/Fundamentals/13.State/src/Components/TodoList.jsx
+++ cursos/yt-50-course/Fundamentals/13.State/src/Components/TodoList.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
   const [lista, setLista] = useState([]);
   const [inputValue, setInputValue] = useState("");
-
-  const handleClick = (e) => {
-    e.preventDefault();
+  const handleSubmite = (e) =>{
+    e.preventDefault()
     if(inputValue.trim()){
       setLista([...lista, inputValue])
       setInputValue("");
     }
   }
-
-  const handleSubmit = (e) => {
-    setInputValue(e.target.value);
+  const handleInput = (e) =>{
+    setInputValue(e.target.value)
   }
-
   return (
     <div>
-      <form onSubmit={handleClick}>
-        <input type="text" placeholder='Digite' value={inputValue} onChange={handleSubmit}/>
+      <form action="post" onSubmit={handleSubmite}>
+        <input type="text" placeholder='Digite' value={inputValue} onChange={handleInput}/>
         <button>Adicionar</button>
       </form>
-      <ul style={{padding: '0'}}>
-        {lista.map((item, index) =>
-          <li key={index} style={{listStyle: "none"}}>{item}</li>
-        )}
-      </ul>
+      <div>
+        <ul>
+          {lista.map((valor, index)=>
+            <li key={index}>{valor}</li>
+          )}
+        </ul>
+      </div>
     </div>
   )
 }
