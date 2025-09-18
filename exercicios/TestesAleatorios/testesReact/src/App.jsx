47a2cca83ede207008ca8b2c5a9cadef0df0b309
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-09-17 23:14:20 -0300

    teste exercicio fetch api

--- exercicios/TestesAleatorios/testesReact/src/App.jsx
+++ exercicios/TestesAleatorios/testesReact/src/App.jsx
@@ -1,26 +1,22 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState([]);
   useEffect(()=>{
     async function getData(){
       const response = await fetch("https://randomuser.me/api")
-      const data = await response.json()
+      const data = await response.json();
       setUser(data.results)
     }
     getData()
-  }, [])
+  },[])
   return (
     <div>
       {user.length > 0 ? (
-        <div>
-          <p>
-            {user[0].name.first}
-          </p>
-        </div>
-      ):(
-        <div>Loading...</div>
-      )}
+        user[0].name.first
+      ): (
+        <p>Loading...</p>
+      )}      
     </div>
   )
 }
