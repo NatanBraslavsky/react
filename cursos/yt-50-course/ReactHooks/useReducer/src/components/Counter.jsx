5b44b4acc6bba790d96eab249722283c2558bdba
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-09-30 09:21:31 -0300

    useReducer exercício

--- cursos/yt-50-course/ReactHooks/useReducer/src/components/Counter.jsx
+++ cursos/yt-50-course/ReactHooks/useReducer/src/components/Counter.jsx
@@ -1,20 +1,45 @@
-import { useReducer } from "react"
-import { initialState, counterReducer } from "../counterReducer"
+import {useReducer, useState} from "react";
+import {initialState, counterReducer} from "../counterReducer";
 
 const Counter = () => {
-    const [state, dispatch] = useReducer(counterReducer, initialState)
+    const [state, dispatch] = useReducer(counterReducer, initialState);
+    const [inputValue, setInputValue] = useState(0);
 
-    const handleIncrement = () =>{dispatch({type:'increment'})}
-    const handleDecrement = () =>{dispatch({type:'decrement'})}
+    const handleIncrement = () => {
+        dispatch({type: "increment"});
+    };
+    const handleDecrement = () => {
+        dispatch({type: "decrement"});
+    };
 
+    const handleIncrementByAmount = () => {
+        dispatch({type: "incrementByAmount", payload: Number(inputValue)});
+        setInputValue(0);
+    };
 
-  return (
-    <div>
-        <h1>{state.count}</h1>
-        <button onClick={handleIncrement}>+</button>
-        <button onClick={handleDecrement}>-</button>
-    </div>
-  )
-}
+    const handleDecrementByAmount = () => {
+        dispatch({type: "decrementByAmount", payload: Number(inputValue)});
+        setInputValue(0);
+    };
 
-export default Counter
+    return (
+        <div>
+            <h1>{state.count}</h1>
+            <button onClick={handleIncrement}>+</button>
+            <button onClick={handleDecrement}>-</button>
+            <div>
+                <input
+                    type="number"
+                    value={inputValue}
+                    onChange={(e) => {
+                        setInputValue(e.target.value);
+                    }}
+                />
+                <button onClick={handleIncrementByAmount}>+</button>
+                <button onClick={handleDecrementByAmount}>-</button>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
