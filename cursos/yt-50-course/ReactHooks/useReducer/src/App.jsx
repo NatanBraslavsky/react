d09fd8f792c81ab43292720698806166d83cf063
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-09-29 21:08:29 -0300

    exercício useReducer

--- cursos/yt-50-course/ReactHooks/useReducer/src/App.jsx
+++ cursos/yt-50-course/ReactHooks/useReducer/src/App.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { useReducer } from 'react'
+
+const reducer = (state, action) =>{
+  switch(action.type){
+    case "Increment":
+      return {...state, count: state.count + 1};
+    case "ShowText":
+      return {...state, showInfo: !state.showInfo}
+    default:
+      return "This Action doens't exist"
+  }
+}
 
 const App = () => {
+
+    const [state, dispatch] = useReducer(reducer, {
+      count: 0,
+      showInfo: true,
+    });
+  // const [count, setCount] = useState(0);
+  // const [showInfo, setShowInfo] = useState(false);
+  const handleClick = () =>{
+    dispatch({type: 'Increment'})
+    dispatch({type: 'ShowText'})
+    // setCount(count+1)
+    // setShowInfo(!showInfo)
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={handleClick}>Increment</button>
+      {state.showInfo && <p>Seja bem vindo! Teste.</p>}
+
+    </div>
   )
 }
 
