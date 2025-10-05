acb764836eafe63cda64ba94e3fb876a70826735
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-10-05 12:20:44 -0300

    feat: add form submit
    adicionei um formulário para funcionar a adição das tarefas ao apertar enter

--- projetos/lista-de-tarefas-2.0/src/components/InputAddTask.jsx
+++ projetos/lista-de-tarefas-2.0/src/components/InputAddTask.jsx
@@ -5,7 +5,8 @@ import "../style/addTask.css";
 const InputAddTask = ({handleChange, addTask}) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const handleSubmit = () =>{
+  const handleSubmit = (e) =>{
+    e.preventDefault()
     if(title.trim()){
       addTask(title,description)
       setTitle("")
@@ -15,14 +16,16 @@ const InputAddTask = ({handleChange, addTask}) => {
 
   return (
     <div className='containerInputsBtnAddTask'>
-      <div className='containerInputsAddTask'>
-        <input type="text" className='inputAdd inputTitle' placeholder='Título da tarefa' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-        <input type="text" className='inputAdd inputDescription' placeholder='Descrição' value={description} onChange={(e)=>setDescription(e.target.value)}/>
-      </div>
-      <div className='containerBtnsAddTask'>
-        <button onClick={handleChange} className='button btnCancel'>Cancelar</button>
-        <button className='button AddTaskBtn' onClick={handleSubmit}>Adicionar Tarefa</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='containerInputsAddTask'>
+          <input type="text" className='inputAdd inputTitle' placeholder='Título da tarefa' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+          <input type="text" className='inputAdd inputDescription' placeholder='Descrição' value={description} onChange={(e)=>setDescription(e.target.value)}/>
+        </div>
+        <div className='containerBtnsAddTask'>
+          <button onClick={handleChange} className='button btnCancel' type='button'>Cancelar</button>
+          <button className='button AddTaskBtn' type='submit'>Adicionar Tarefa</button>
+        </div>
+      </form>
     </div>
   )
 }
