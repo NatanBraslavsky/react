af2b23e4be002eced2dfcab75c82f2a0fd5a2b2d
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-10-06 21:55:26 -0300

    feat: adicionei foco no input ao enviar o formulario

--- projetos/lista-de-tarefas-2.0/src/components/InputAddTask.jsx
+++ projetos/lista-de-tarefas-2.0/src/components/InputAddTask.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "../style/index.css";
 import "../style/addTask.css";
 
 const InputAddTask = ({handleChange, addTask}) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const inputRef = useRef(null);
   const handleSubmit = (e) =>{
     e.preventDefault()
     if(title.trim()){
       addTask(title,description)
       setTitle("")
       setDescription("")
+      inputFocus();
     }
+  }
+  const inputFocus = () =>{
+    inputRef.current.focus();
   }
 
   return (
     <div className='containerInputsBtnAddTask'>
       <form onSubmit={handleSubmit}>
         <div className='containerInputsAddTask'>
-          <input type="text" className='inputAdd inputTitle' placeholder='Título da tarefa' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+          <input type="text" className='inputAdd inputTitle' placeholder='Título da tarefa' value={title} onChange={(e)=>setTitle(e.target.value)} ref={inputRef}/>
           <input type="text" className='inputAdd inputDescription' placeholder='Descrição' value={description} onChange={(e)=>setDescription(e.target.value)}/>
         </div>
         <div className='containerBtnsAddTask'>
