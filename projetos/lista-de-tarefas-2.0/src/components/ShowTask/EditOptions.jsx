1b3591cf6ec483e439c0a34f564dec3455cdf9bf
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-10-09 09:35:18 -0300

    refactor: ajustei posicionamento edit option
    . Mudei um icone
    . Ajustei posicionamento

--- projetos/lista-de-tarefas-2.0/src/components/ShowTask/EditOptions.jsx
+++ projetos/lista-de-tarefas-2.0/src/components/ShowTask/EditOptions.jsx
@@ -1,4 +1,4 @@
-import { PiPencilSimpleLineLight } from "react-icons/pi";
+import { GoPencil } from "react-icons/go";
 import { GoTrash } from "react-icons/go";
 import {IoDuplicateOutline} from "react-icons/io5";
 
@@ -8,16 +8,16 @@ const EditOptions = () => {
     return (
         <div className="containerEditOptions">
             <div className="selectAllContainer containerEditTask">
-                <PiPencilSimpleLineLight className="iconAll pencilIcon"/> 
+                <GoPencil className="iconAll pencilIcon"/> 
                 <span className="txtAll editTxt">Editar</span>
-            </div>
-            <div className="selectAllContainer containerDeleteTask">
-                <GoTrash className="iconAll trashIcon"/> 
-                <span className="txtAll deleteTxt">Excluir</span>
             </div>
             <div className="selectAllContainer containerDuplicateTask">
                 <IoDuplicateOutline className="iconAll duplicateIcon"/>
                 <span className="txtAll duplicateTxt">Duplicar</span>
+            </div>
+            <div className="selectAllContainer containerDeleteTask">
+                <GoTrash className="iconAll trashIcon"/> 
+                <span className="txtAll deleteTxt">Excluir</span>
             </div>
         </div>
     );
