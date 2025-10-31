3362ead59fe4316945a7e68ada1ecdebb12bf5b1
Natan Braslavsky <natanbraslavsky1@gmail.com>
2025-10-31 17:51:21 -0300

    feat: Término editTask com as funcionalidades já implementadas

--- projetos/lista-de-tarefas-2.0/src/components/EditTask/ModalTask.jsx
+++ projetos/lista-de-tarefas-2.0/src/components/EditTask/ModalTask.jsx
@@ -3,7 +3,7 @@ import '../../style/EditTask/editTask.css'
 import { GoInbox } from 'react-icons/go';
 import { GrFormClose } from 'react-icons/gr';
 
-const ModalTask = ({task, setSelectedTask}) => {
+const ModalTask = ({task, setSelectedTask, editTask}) => {
 
   const [title, setTitle] = useState(task.title);
   const [description, setDescription] = useState(task.description);
@@ -11,6 +11,15 @@ const ModalTask = ({task, setSelectedTask}) => {
   const handleClose = (e) => {
     if(e.target === e.currentTarget){
       setSelectedTask(null)
+    }
+  }
+  const handleSave = () => {
+    if(title.trim()){
+      editTask(task.id, {
+        title: title,
+        description: description
+      })
+      setSelectedTask(null);
     }
   }
 
@@ -29,12 +38,12 @@ const ModalTask = ({task, setSelectedTask}) => {
         </div>
         <div className="containerInputsBtnsEditTask">
           <div className='containerTitleDescEditTask'>
-            <input type="text" value={title} className='titleEditTaskInput' placeholder='Nome da tarefa'/>
-            <textarea type="text" value={description} className='descriptionEditTaskInput' placeholder='Descrição'/>
+            <input type="text" value={title} className='titleEditTaskInput' placeholder='Nome da tarefa' maxLength={50} onChange={(e) => setTitle(e.target.value)}/>
+            <textarea type="text" value={description} className='descriptionEditTaskInput' placeholder='Descrição' maxLength={100} onChange={(e) => setDescription(e.target.value)}/>
           </div>
           <div className='containerBtns'>
             <button className='btnEditTask btnCancel' onClick={handleClose}>Cancelar</button>
-            <button className='btnEditTask btnSave'>Salvar</button>
+            <button className='btnEditTask btnSave' onClick={handleSave}>Salvar</button>
           </div>
         </div>
       </div>
